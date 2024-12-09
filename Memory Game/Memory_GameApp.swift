23351ef9f8eba1c1d6892d6c5afc8f27cecd42b4
JonathanVarghese <jvarghese@bsd220.org>
2024-12-09 09:31:21 -0600

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Jonathan V on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory GameTests/Memory_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  Memory_GameTests.swift
+//  Memory GameTests
+//
+//  Created by Jonathan V on 12/9/24.
+//
+
+import Testing
+
+struct Memory_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
